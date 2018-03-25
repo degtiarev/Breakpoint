bf6a9e6bce80b5fc80cc7622ee5b562784376b8b
Aleksei Degtiarev <delexa0@gmail.com>
2018-03-25 14:15:18 +0200

    Added posting feed messages

---
+++ breakpoint/Controller/CreatePostVC.swift
@@ -0,0 +1,60 @@
+//
+//  CreatePostVC.swift
+//  breakpoint
+//
+//  Created by Aleksei Degtiarev on 25/03/2018.
+//  Copyright © 2018 Aleksei Degtiarev. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class CreatePostVC: UIViewController {
+    
+    
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var sendButton: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        textView.delegate = self
+    }
+    
+    
+    
+    @IBAction func sendButtonPressed(_ sender: UIButton) {
+        
+        if textView.text != "" && textView.text != "Say something here..." {
+            sendButton.isEnabled = false
+            DataService.instance.uploadPost(withMessage: textView.text!, forUID: (Auth.auth().currentUser?.uid)!, withGroupKey: nil, sendComplete: { (isComplete) in
+                if isComplete {
+                    self.sendButton.isEnabled = true
+                    self.dismiss(animated: true, completion: nil)
+                } else {
+                    self.sendButton.isEnabled = true
+                    print("There was an error!")
+                }
+            })
+        }
+    }
+    
+    
+    @IBAction func closeButtonPressed(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
+
+
+
+
+extension CreatePostVC: UITextViewDelegate {
+    
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        textView.text = ""
+    }
+}
--- breakpoint/Services/DataService.swift
+++ breakpoint/Services/DataService.swift
@@ -40,5 +40,13 @@ class DataService {
         REF_USERS.child(uid).updateChildValues(userData)
     }
     
+    func uploadPost(withMessage message: String, forUID uid: String, withGroupKey groupKey: String?, sendComplete: @escaping (_ status: Bool) -> ()) {
+        if groupKey != nil {
+            // send to group ref
+        } else {
+            _REF_FEED.childByAutoId().updateChildValues(["content": message, "senderID": uid])
+            sendComplete(true)
+        }
+    }
     
 }
