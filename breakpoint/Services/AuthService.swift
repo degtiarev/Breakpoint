625441475e44231aec8330920b8ad1d4d993ca98
Aleksei Degtiarev <delexa0@gmail.com>
2018-03-25 05:14:41 +0200

    Added Authorisation service

---
+++ breakpoint/Services/AuthService.swift
@@ -0,0 +1,47 @@
+//
+//  Auth.swift
+//  breakpoint
+//
+//  Created by Aleksei Degtiarev on 25/03/2018.
+//  Copyright © 2018 Aleksei Degtiarev. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+class AuthService {
+    
+    static let instance = AuthService()
+    
+    func registerUser(withEmail email: String, andPassword password: String, userCreationComplete: @escaping(_ status: Bool, _ error: Error?) ->()) {
+        
+        Auth.auth().createUser(withEmail: email, password: password) { (user, error) in
+            guard let user = user else {
+                userCreationComplete(false, error)
+                return
+            }
+            
+            
+            let userDara = ["provider": user.providerID, "email": user.email ]
+            DataService.instance.createDBUser(uid: user.uid, userData: userDara)
+            userCreationComplete(true, nil)
+            
+        }
+    } // func registerUser(withEmail email: ...
+    
+    
+    func loginUser(withEmail email: String, andPassword password: String, loginComplete: @escaping(_ status: Bool, _ error: Error?) ->()) {
+        
+        Auth.auth().signIn(withEmail: email, link: password) { (result, error) in
+            
+            if error != nil {
+                loginComplete(false, error)
+                return
+            }
+            loginComplete(true, nil)
+            
+        }
+    } // func loginUser(withEmail email:
+    
+}
+
