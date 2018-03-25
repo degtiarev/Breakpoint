8e57785b110c069ddc8f7b6c466c26e9bc65aa8b
Aleksei Degtiarev <delexa0@gmail.com>
2018-03-25 10:15:57 +0200

    Added view and Viewcontroller for user info

---
+++ breakpoint/Controller/MeVC.swift
@@ -0,0 +1,26 @@
+//
+//  MeVC.swift
+//  breakpoint
+//
+//  Created by Aleksei Degtiarev on 25/03/2018.
+//  Copyright © 2018 Aleksei Degtiarev. All rights reserved.
+//
+
+import UIKit
+
+class MeVC: UIViewController {
+    
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    @IBAction func signOutButtonPressed(_ sender: Any) {
+    }
+    
+}
